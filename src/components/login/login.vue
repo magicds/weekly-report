66520fbebc33f4f0b5228922bad4ff3a4a557b4d
cdswyda <cdswyda@hotmail.com>
2017-09-26 17:06:20 +0800

    add login

---
+++ src/components/login/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login-wrap">
+      <div class="login-box">
+          <form>
+            <label for="user-name">姓名</label><input id="user-name" name="user-name" type="text" v-model="name" placeholder="请输入姓名">
+            <label for="user-pwd">密码</label><input type="password" id="user-pwd" name="user-pwd" v-model="pwd" placeholder="请输入密码">
+            <input type="button" value="登录" @click="login">
+          </form>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      name: '',
+      pwd: ''
+    }
+  },
+  methods:{
+      login() {
+          console.log(this);
+          
+      }
+  }
+}
+
+</script>
+
+<style >
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Login from '@/components/login/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Hello',
+    //   component: Hello
+    // },
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path:'/',
+      name:'Login',
+      component: Login
     }
   ]
 })
