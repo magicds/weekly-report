0ecaf3da2ac7a6390f28c9d11e4065c899d0a9ba
cdswyda <cdswyda@hotmail.com>
2017-10-30 16:11:50 +0800

    some apis

--- src/api/data.js
+++ src/api/data.js
@@ -1,2 +1,120 @@
 import AV from 'leancloud-storage'
+import moment from 'moment'
 
+import throwError from './error.js'
+
+var dataApi = {
+  /**
+   * 获取数据
+   *
+   * @param {String} cls 要查询的class名称
+   * @param {Object/Array} conditions 查询条件
+   * 每个成员格式为：
+   * {
+   *     action:'操作名称，可选值为equalTo、notEqualTo、greaterThan、greaterThanOrEqualTo、lessThan、lessThanOrEqualTo',
+   *     field:'条件字段名称',
+   *     value:'值'
+   * }
+   * @param {Object/Array} sorts 排序规则，如 [{"sort":"asc","field":"groupId"},{"sort":"desc","field":"memberIndex"}]
+   * @returns
+   */
+  getData(cls, conditions, sorts) {
+    let query = new AV.query(cls)
+
+    if (conditions) {
+      if (!(conditions instanceof Array)) {
+        conditions = [conditions]
+      }
+      conditions.forEach(function (item) {
+        query[item.action](item.field, item.value)
+      })
+    }
+
+    if (sorts) {
+      if (!(sorts instanceof Array)) {
+        sorts = [sorts]
+      }
+
+      sorts.forEach(function (item) {
+        var sort = item.sort.toLowerCase()
+        if (sort == 'asc') {
+          item.field && query.addAscending(item.field)
+        } else if (sort == 'desc') {
+          item.field && query.addDescending(item.field)
+        }
+      })
+    }
+
+    return query.find().catch(throwError)
+  },
+  /**
+   * 查询本周的周报
+   * @param {Object/Array} sorts 排序规则
+   */
+  getCurrWeekData(sorts) {
+    // 根据当天 获取周一和周日
+    let date = moment()
+      .hour(0)
+      .minute(0)
+      .second(0)
+      .millisecond(0)
+    let day = date.isoWeekday()
+    let startDate = date
+      .clone()
+      .subtract(day - 1, 'days')
+      .toDate()
+    // 结束时间直接取到下周一凌晨
+    let endDate = date
+      .clone()
+      .add(8 - day, 'days')
+      // .hour(23)
+      // .minute(59)
+      // .second(59)
+      // .millisecond(999)
+      .toDate()
+
+    console.log(startDate, endDate)
+
+    let conditions = [{
+        action: 'greaterThanOrEqualTo',
+        field: 'updatedAt',
+        value: startDate
+      },
+      {
+        action: 'lessThanOrEqualTo',
+        field: 'updatedAt',
+        value: endDate
+      }
+    ]
+    return this.getData('', sorts)
+  },
+  /**
+   * 新增数据
+   * @param {String} cls 存储class名称
+   * @param {Object} data 存储数据
+   */
+  addData(cls, data) {
+    let ObjCls = AV.extend(cls)
+    let obj = new ObjCls()
+    let key
+    for (key in data) {
+      if (data.hasOwnProperty(key)) {
+        obj.set(key, data[key])
+      }
+    }
+
+    return obj.save().catch(throwError)
+  },
+  addReprot(data) {
+    // 自动为数据加入当前用户
+    data.user = {
+      id: '',
+      name: '',
+      group: ''
+    }
+
+    return this.addData('', data)
+  }
+}
+
+export default dataApi
--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,55 @@
 import AV from 'leancloud-storage'
+import throwError from './error.js'
+import dataApi from './data.js'
 
 export default {
-  login (name,pwd) {
-    return AV.User.logIn(name,pwd);
-  }
+  login(name, pwd) {
+    return AV.User.logIn(name, pwd)
+  },
+  signUp(name, pwd, email) {
+    let user = new AV.User()
+
+    user.setUsername(name)
+    user.setPassword(pwd)
+    user.setEmail(email)
+
+    return user
+      .save()
+      .then(user => {
+        // 注册成功后查询是否为第一个用户 为其添加 管理员角色
+        return this.getUsersCount().then(count => {
+          if (count > 1) return
+          this.addAdminRole(user)
+        })
+      })
+      .catch(throwError)
+  },
+  getUsersCount() {
+    return new AV.Query('_Users').count().catch(throwError)
+  },
+  /**
+   * 为当前用户添加 admin 角色
+   * @param {Object} user
+   * @returns {Promise}
+   */
+  addAdminRole(user) {
+    // 新建acl权限控制
+    let roleAcl = new AV.ACL()
+    // 允许公共读、禁止公共写
+    roleAcl.setPublicReadAccess(true)
+    roleAcl.setPublicWriteAccess(false)
+
+    // 为此用户加入自己的写权限
+    roleAcl.setWriteAccess(user, true)
+
+    // 新建角色 并赋值acl
+    let administratorRole = new AV.Role('Administrator', roleAcl)
+    return administratorRole
+      .save()
+      .then(role => {
+        console.log(role, '成功添加给', user)
+      })
+      .catch(throwError)
+  },
+  getAllUsers() {}
 }
