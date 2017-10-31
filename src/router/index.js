9742b199fe683b6ab65b6ffc3dd24628a80d1ae4
cdswyda <cdswyda@hotmail.com>
2017-10-31 16:39:06 +0800

    调整路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/components/login'
-import Signup from "@/components/signup"
-import Main from "@/components/main"
+import Signup from '@/components/signup'
+import Main from '@/components/main'
+import Input from '@/components/input'
 
 Vue.use(Router)
 
@@ -23,7 +24,18 @@ export default new Router({
       path: '/main',
       name: 'Main',
       component: Main,
-
+      children: [
+        {
+          path: '',
+          name: 'input',
+          component: Input
+        },
+        {
+          path: 'input',
+          name: 'input',
+          component: Input
+        }
+      ]
     },
     {
       path: '/signup',
