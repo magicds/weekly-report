60fb13c377ee33a74688e06177a1f1bc2d300a24
cdswyda <cdswyda@hotmail.com>
2017-11-01 17:22:43 +0800

    add group config

---
+++ src/config/group.config.js
@@ -0,0 +1,16 @@
+export default {
+  groups: [
+    {
+      index: 0,
+      name: 'Front End Support'
+    },
+    {
+      index: 1,
+      name: 'Front End PC'
+    },
+    {
+      index: 2,
+      name: 'Front End Mobile'
+    }
+  ]
+}
