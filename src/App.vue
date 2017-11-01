0533f128c53e7ed00162cd8a2443011008658363
cdswyda <cdswyda@hotmail.com>
2017-11-01 16:03:12 +0800

    some style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <!-- <img src="./assets/logo.png"> -->
     <router-view></router-view>
   </div>
@@ -12,6 +12,36 @@ export default {
 </script>
 
 <style>
+.container,
+.container-fixed,
+.container-fixed-md,
+.container-fixed-sm,
+.container-fixed-xs,
+.container-fluid {
+  padding-right: 10px;
+  padding-left: 10px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+@media (min-width: 768px) {
+  .container {
+    max-width: 740px;
+  }
+}
+
+@media (min-width: 992px) {
+  .container {
+    max-width: 960px;
+  }
+}
+
+@media (min-width: 1200px) {
+  .container {
+    max-width: 1160px;
+  }
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
