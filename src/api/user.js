4cbb32ca5b94ec82044b80e4e3b4e98e25a42a27
cdswyda <cdswyda@hotmail.com>
2017-11-01 17:36:25 +0800

    调整了第一个用户创建后的逻辑，新加入了创建小组

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,7 @@
 import AV from 'leancloud-storage'
 import throwError from './error.js'
+import groupInfo from '@/config/group.config.js'
+import dataApi from './data.js'
 import Promise from 'bluebird'
 
 window.AV = AV
@@ -27,19 +29,29 @@ export default {
         console.log('注册成功,当前用户为', AV.User.current())
         // 注册成功后查询是否为第一个用户 为其添加 管理员角色
         return this.getUsersCount().then(count => {
-          let rolePromise = []
+          // let promArr = []
 
-          if (count > 1) {
-            rolePromise.push(this.addRole('normal', user))
+          // if (count > 1) {
+          //   promArr.push(this.addRole('normal', user))
+          // } else {
+          //   // 第一个用户进来创建角色
+          //   promArr.push(this.createRole('administrator', user))
+          //   promArr.push(this.createRole('normal', user))
+
+          // }
+
+          // // 角色创建及相关初始化完成或添加完毕后 为用户添加角色权限控制
+          // Promise.all(promArr).then(data => {
+          //   this.afterUserSignUp(user)
+          // })
+
+          if (count > 0) {
+            return this.addRole('normal', user).then(data => {
+              return this.afterUserSignUp(user)
+            })
           } else {
-            rolePromise.push(this.createRole('administrator', user))
-            rolePromise.push(this.createRole('normal', user))
+            return this.initApp(user)
           }
-
-          // 角色创建或添加完毕后 为用户添加角色权限控制
-          Promise.all(rolePromise).then(data => {
-            this.afterUserSignUp(user)
-          })
         })
       })
       .catch(throwError)
@@ -127,5 +139,34 @@ export default {
       })
       .catch(throwError)
   },
+  // 初始化创建小组
+  initGroups() {
+    let arr = []
+
+    if (groupInfo.group && groupInfo.group.length) {
+      groupInfo.group.forEach(item => {
+        arr.push(dataApi.addData('Group', item))
+      })
+    }
+
+    return arr
+  },
+  // 第一个用户注册后的后续操作
+  // 先创建两个决定
+  // 之后再 初始化创建小组、将角色权限加到此用户身上
+  initApp(user) {
+    let rolePromises = [
+      this.createRole('administrator', user),
+      this.createRole('normal', user)
+    ]
+    // 角色创建完成
+    return Promise.all(rolePromises).then(() => {
+      // 创建小组和角色权限
+      let arr = [].concat(this.initGroups())
+      arr.push(this.afterUserSignUp(user))
+      return Promise.all(arr)
+    })
+  },
+
   getAllUsers() {}
 }
