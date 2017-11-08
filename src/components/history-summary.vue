47aaac35028867fded0d1f28fe757a5e6d8b2b26
cdswyda <cdswyda@hotmail.com>
2017-11-08 17:01:13 +0800

    新增历史查看

---
+++ src/components/history-summary.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="histroysmmmary">
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'histroysmmmary',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+
+</style>
