2e4ea96b04b3e8a647490328907752dcd2581317
cdswyda <cdswyda@gmail.com>
2018-08-12 14:33:57 +0800

    fixed #5 add 全局错误提醒

--- src/api/error.js
+++ src/api/error.js
@@ -1,4 +1,11 @@
+import Message from 'iview/src/components/message';
+
 export default function throwError(err) {
   console.log(err);
+  Message.error({
+    content: `<pre>${JSON.stringify(err, 0, 4)}</pre>`,
+    closable: true,
+    duration: 5
+  });
   throw err;
 }
